01d3fb28863ed829c128da1a8a1f5f395c715e19
wshahbaz <wais.shahbaz@gmail.com>
2021-01-17 15:34:05 -0500

    created passive aggressive classifier and tfidf vectorizer, generated predictions

---
+++ model_pred.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+#Read the data
+df=pd.read_csv('news.csv')
+
+#Get shape and head
+df.shape
+df.head()
+
+#Get the labels
+labels=df.label
+labels.head()
+
+#Split the dataset
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
+
+#Initialize a TfidfVectorizer
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+
+#Fit and transform train set, transform test set
+tfidf_train=tfidf_vectorizer.fit_transform(x_train)
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+#Initialize a PassiveAggressiveClassifier
+pac=PassiveAggressiveClassifier(max_iter=50, verbose=1)
+pac.fit(tfidf_train,y_train)
+
+#Predict on the test set and calculate accuracy
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print()
+print(f'Accuracy: {round(score*100,2)}%')
+
+#Build a confusion matrix
+def plot_confusion_matrix(cm,
+                          target_names,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True):
+
+    import matplotlib.pyplot as plt
+    import itertools
+
+    accuracy = np.trace(cm) / np.sum(cm).astype('float')
+    misclass = 1 - accuracy
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(8, 6))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
+    plt.show()
+
+#plot confusion matrix
+plot_confusion_matrix(confusion_matrix(y_test,y_pred), ['FAKE','REAL'], normalize=False)
